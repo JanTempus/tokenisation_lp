5e2c63ce1e05d251fb563890b2ba1cb35b56e054
Jan Tempus <jan.tempus@gmail.com>
2025-06-14 11:45:40 +0200

    It prints more things now

--- stress_testing.py
+++ stress_testing.py
@@ -18,17 +18,23 @@ print("Downloaded the data set")
 corpus=[]
 
 
-for i in range(len(TinyStories['train'])):
+dataSetSize=len(TinyStories['train'])
+
+for i in range(dataSetSize):
     corpus.append(TinyStories['train'][i]['text'])
 
 word_freqs = defaultdict(int)
 
 print("created the corpus")
+
+i=0
 for text in corpus:
+    print(i, " out of ", dataSetSize)
     words_with_offsets = tokenizer.backend_tokenizer.pre_tokenizer.pre_tokenize_str(text)
     new_words = [word for word, offset in words_with_offsets]
     for word in new_words:
         word_freqs[word] += 1
+    i=i+1
 
 word_freqs={k: v for k, v in word_freqs.items() if len(k) > 1}
 
