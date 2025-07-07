8b8c3b79a2b87b99db85dee1595a88d9fa73d3e8
Jan Tempus <jan.tempus@gmail.com>
2025-07-07 11:31:08 +0200

    increased data set size

--- load_and_prepare_dataset.py
+++ load_and_prepare_dataset.py
@@ -43,7 +43,7 @@ corpus=[]
 
 dataSetSize=len(TinyStories['train'])
 
-dataSetSize = int(dataSetSize/2)
+dataSetSize = int(dataSetSize)
 
 for i in range(dataSetSize):
     corpus.append(TinyStories['train'][i]['text'])
