bd314a31ac918c852b37812ccb6c88d3f924952c
Jan Tempus <jan.tempus@gmail.com>
2025-08-15 10:41:57 +0200

    printing things now

--- lp_tokenizer/measuring_intrinstics.py
+++ lp_tokenizer/measuring_intrinstics.py
@@ -85,6 +85,7 @@ while dataset_size<dataset_size_max:
         vocab=tokenizer.get_vocab()
 
         compression=tokenizer.encode(corpus, vocab)
+        print(f"dataset_size {dataset_size } vocab size {vocab_size} compression {compression}  ")
         save_data(intristics_path,dataset_size,vocab_size,compression)
         vocab_size_dif=vocab_size_dif*2
 
