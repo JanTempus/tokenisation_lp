426a9a92905ba114a99b7ce45d6b699fd6ed7e74
Jan Tempus <jan.tempus@gmail.com>
2025-08-16 12:45:21 +0200

    issues

--- lp_tokenizer/lp_tokenizer.py
+++ lp_tokenizer/lp_tokenizer.py
@@ -161,10 +161,10 @@ class Tokenizer:
         if self.unk_token is None:
             raise KeyError("Please assign a token to the unkown token")
 
-    
+        pretokenizer = AutoTokenizer.from_pretrained("/path/to/local/pythia-70m-deduped")
         input_strings=[]
         for i, text in tqdm(enumerate(corpus), total=len(corpus), desc="Pretokenizing"):
-            words_with_offsets = self.pretokenizer.backend_tokenizer.pre_tokenizer.pre_tokenize_str(text)
+            words_with_offsets = pretokenizer.backend_tokenizer.pre_tokenizer.pre_tokenize_str(text)
             new_words = [word for word, offset in words_with_offsets]
             input_strings+=new_words
     
