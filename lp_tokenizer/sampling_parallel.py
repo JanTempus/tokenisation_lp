32f51f132590e6f53c0ef99eef0ddc7d03bba5e4
Jan Tempus <jan.tempus@gmail.com>
2025-08-22 10:06:36 +0200

    lets see if this works

---
+++ lp_tokenizer/sampling_parallel.py
@@ -0,0 +1,58 @@
+from lp_tokenizer import Tokenizer
+from transformers import AutoTokenizer
+from datasets import  load_from_disk,load_dataset, Dataset
+import os
+import csv
+from tqdm import tqdm
+import numpy as np
+from functools import partial
+
+def collect_pretokenized_words(dataset, pretokenizer, t: int, num_proc: int = 1):
+    """
+    Pretokenize the first t examples of a Hugging Face dataset in parallel
+    and return all words as a single list.
+    """
+
+    # restrict dataset to the first t examples
+    dataset = dataset['train'].select(range(t))
+
+    # define a per-example processing function
+    def process(example):
+        words_with_offsets = pretokenizer.backend_tokenizer.pre_tokenizer.pre_tokenize_str(example['text'])
+        new_words = [word for word, offset in words_with_offsets]
+        return {"words": new_words}
+
+    # run map in parallel
+    processed = dataset.map(
+        process,
+        remove_columns=dataset.column_names,
+        desc="Pretokenizing",
+        num_proc=num_proc,
+    )
+
+    # flatten all lists of words into one list
+    all_words = sum(processed["words"], [])
+
+    return all_words
+
+
+num_proc = 8
+
+datasetname="finewebedu"
+dataset_url="pietrolesci/finewebedu-20B"
+dataset_path="finewebedu_data"
+
+pretokenizer=AutoTokenizer.from_pretrained("EleutherAI/pythia-70m-deduped",
+                              revision="step3000",
+                              cache_dir="./pythia-70m-deduped/step3000",
+                                            )
+
+dataset_size=8192 #1048576
+
+dataset=load_from_disk(dataset_path)
+
+all_words = collect_pretokenized_words(dataset, pretokenizer, t=8192, num_proc=4)
+print(len(all_words), "words collected")
+print(all_words[:50])
+
+   
--- lp_tokenizer/sampling_sentences.py
+++ lp_tokenizer/sampling_sentences.py
@@ -8,6 +8,13 @@ import numpy as np
 from functools import partial
 
 def collect_pretokenized_words(corpus, pretokenizer):
+
+
+    corpus=[]
+
+    for i in tqdm(range(dataset_size),desc="Appending text to the corpus"):
+        corpus.append(dataset['train'][i]['text'])
+
     all_words = []
 
     for i, text in tqdm(enumerate(corpus), total=len(corpus), desc="Pretokenizing"):
@@ -15,8 +22,27 @@ def collect_pretokenized_words(corpus, pretokenizer):
         new_words = [word for word, offset in words_with_offsets]
         all_words.extend(new_words)  # collect all words
 
-    print(len(all_words))
+    #print(len(all_words))
+    return all_words
 
+
+def merge_into_chunks(dataset, t: int,):
+
+
+    merged_texts = []
+    # Go through dataset in steps of t
+    for i in range(0, len(dataset), t):
+        chunk = dataset[i : i + t]  # list of texts
+        merged_text = " ".join(chunk)
+        merged_texts.append(merged_text)
+
+    # Create new dataset
+    dataset_merged = Dataset.from_dict({'text': merged_texts})
+    return dataset_merged
+
+
+
+num_proc = 8
 
 datasetname="finewebedu"
 dataset_url="pietrolesci/finewebedu-20B"
@@ -36,11 +62,6 @@ if dataset_url is None and dataset_path is None:
 
 
 dataset=load_from_disk(dataset_path)
-
-corpus=[]
-
-for i in tqdm(range(dataset_size),desc="Appending text to the corpus"):
-    corpus.append(dataset['train'][i]['text'])
 
 
 collect_pretokenized_words(corpus, pretokenizer)
