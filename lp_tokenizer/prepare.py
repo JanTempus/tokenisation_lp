510a2da03e7fd99dfc3dc47af21a1ff32a304b2e
Jan Tempus <jan.tempus@gmail.com>
2025-08-24 16:47:20 +0200

    more processors

--- lp_tokenizer/prepare.py
+++ lp_tokenizer/prepare.py
@@ -9,8 +9,7 @@ from datasets import load_dataset,load_from_disk # huggingface datasets
 from lp_tokenizer import Tokenizer
 # number of workers in .map() call
 # good number to use is ~order number of cpu cores // 2
-num_proc = 1
-
+num_proc = 8
 # number of workers in load_dataset() call
 # best number might be different from num_proc above as it also depends on NW speed.
 # it is better than 1 usually though
