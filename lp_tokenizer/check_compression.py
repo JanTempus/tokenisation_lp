4a7f3edbeb6c8b99c008933357fef90f88bc5ca2
Jan Tempus <jan.tempus@gmail.com>
2025-08-25 17:17:49 +0200

    eugh

--- lp_tokenizer/check_compression.py
+++ lp_tokenizer/check_compression.py
@@ -13,7 +13,7 @@ dataset_size=65536
 vocab_size=1024
 
 
-file_path=f"vocabs/vocab_finewebedu_data_{vocab_size}.json"
+file_path=f"vocabs/vocab_finewebedu_data_0_{vocab_size}.json"
 
 with open(file_path, 'r', encoding='utf-8') as f:
         vocab = json.load(f)
