55711518d5b56f4ece72e20920d8d54a61460611
Jan Tempus <jan.tempus@gmail.com>
2025-08-26 11:59:52 +0200

    maybe less io

--- lp_tokenizer/lp_functions.py
+++ lp_tokenizer/lp_functions.py
@@ -251,7 +251,7 @@ def tokenize(edgesList: list[list[tokenInstance]] ,
 
     problem = cp.Problem(objective, constraints)
  
-    problem.solve(solver=cp.CUOPT,verbose=False)
+    problem.solve(solver=cp.CUOPT,CUOPT_LOG_TO_CONSOLE=True)
  
     flow_values = f.value 
     shortest_paths = []
