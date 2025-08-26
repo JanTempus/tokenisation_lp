a337cfb13271f81db5d2295a6ea6b1a88e3b2b14
Jan Tempus <jan.tempus@gmail.com>
2025-08-26 11:54:42 +0200

    clean up

---
+++ lp_tokenizer/train_tokenizer.py
@@ -0,0 +1,37 @@
+
+from lp_tokenizer import Tokenizer
+
+from datasets import  load_from_disk,load_dataset
+import pickle
+import os
+
+
+
+# datasetname="tinystories"
+# dataset_url="roneneldan/TinyStories"
+# dataset_path="tinystories_data"
+
+
+datasetname="finewebedu"
+dataset_url="pietrolesci/finewebedu-20B"
+dataset_path="finewebedu_data"
+
+tokenizer=Tokenizer(dataset_url, dataset_path,2000,32000)
+
+tokenizer.make_vocab()
+
+
+
+# dataset=load_from_disk(dataset_path)
+
+# corpus=[]
+
+# for i in range(4,8):
+#     corpus.append(dataset['train'][i]['text'])
+
+# vocab=tokenizer.get_vocab()
+
+# tokenized_data=tokenizer.encode(corpus,vocab)
+# print(tokenized_data)
+
+
